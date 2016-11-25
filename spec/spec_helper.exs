a6cf92b0be91e12cf6f01326692e0cf367694b34
Marnen Laibow-Koser <marnen@marnen.org>
2016-11-25 17:10:13 -0500

    Remaining espec_phoenix configuration. [#2]

--- spec/spec_helper.exs
+++ spec/spec_helper.exs
@@ -1,9 +1,12 @@
+Code.require_file("#{__DIR__}/phoenix_helper.exs")
+
 ESpec.configure fn(config) ->
-  config.before fn(tags) ->
-    {:shared, hello: :world, tags: tags}
+  config.before fn(_tags) ->
+    # {:shared, hello: :world, tags: tags}
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(YourApp.Repo)
   end
 
   config.finally fn(_shared) ->
-    :ok
+    Ecto.Adapters.SQL.Sandbox.checkin(YourApp.Repo, [])
   end
 end
